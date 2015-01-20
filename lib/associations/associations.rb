7bb3bac30ff48a31f9a4a63cf5d51c4a94f27abf
maukoquiroga <maukoquiroga@gmail.com>
2015-01-20 14:29:24 +0100

    has_many :through

--- lib/associations/associations.rb
+++ lib/associations/associations.rb
@@ -97,11 +97,14 @@ module ActiveHash
           klass = options[:class_name].constantize
           primary_key_value = send(options[:primary_key])
           foreign_key = options[:foreign_key].to_sym
+          through = options[:through]
 
           if Object.const_defined?(:ActiveRecord) && ActiveRecord.const_defined?(:Relation) && klass < ActiveRecord::Relation
             klass.where(foreign_key => primary_key_value)
           elsif klass.respond_to?(:scoped)
             klass.scoped(:conditions => {foreign_key => primary_key_value})
+          elsif through
+            klass.joins(through).where(through => {foreign_key => primary_key_value})
           else
             klass.where(foreign_key => primary_key_value)
           end
